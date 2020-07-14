72e63d901a9f26456e9e3b5066796458e8eab278
thiago orlandini <thiagorlandini@hotmail.com>
2020-07-14 08:30:34 -0300

    criando o projeto

---
+++ src/main/java/br/edu/infnet/javainjecaopersistenciaemprestimos/JavaInjecaoPersistenciaEmprestimosApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.javainjecaopersistenciaemprestimos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaInjecaoPersistenciaEmprestimosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaInjecaoPersistenciaEmprestimosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/javainjecaopersistenciaemprestimos/JavaInjecaoPersistenciaEmprestimosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.javainjecaopersistenciaemprestimos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaInjecaoPersistenciaEmprestimosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
